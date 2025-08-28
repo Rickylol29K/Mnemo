eeaaeeb5467ce3dcf1c2b84b6f6842793656cfb5
Rickylol29K <petersrick15@gmail.com>
2025-08-28 18:04:29 +0200

    Version 1.2

---
+++ ai-study-ui/src/components/QuizBlock.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+import Quiz from "@/components/Quiz";
+
+export default function QuizBlock() {
+  // simple wrapper so existing imports in page.tsx resolve
+  return <Quiz />;
+}
+
